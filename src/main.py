61ed7481091de704cc4803f646244d6c3a7f38ce
Ayala <ayalafreire7@gmail.com>
2026-03-30 10:27:00 -0300

    feat: cria esqueleto inicial do pipeline principal em python

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def carregar_dados():
+    pass
+
+
+def transformar_dados(df):
+    pass
+
+
+def gerar_relatorios(df):
+    pass
+
+
+def salvar_dados(df):
+    pass
+
+
+def main():
+    print("Projeto final iniciado com sucesso.")
+
+
+if __name__ == "__main__":
+    main()
