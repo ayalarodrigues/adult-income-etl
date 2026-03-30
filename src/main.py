a417741384c923d50b9d4c53eed7ef7050c72270
Ayala <ayalafreire7@gmail.com>
2026-03-30 11:12:27 -0300

    feat: adiciona inspecao inicial com head, shape, info e describe

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 def carregar_dados():
 
+    '''Etapa de extração.
+    Lê o arquivi principal da base adult e retorna um DataFrame'''
+
     print("\n --- FASE DE EXTRAÇÃO--- \n")
 
     df = pd.read_csv("data/raw/adult.csv") #lê o arquivo CSV e cria uma Dataframe
@@ -26,8 +29,26 @@ def salvar_dados(df):
 def main():
     df = carregar_dados()
     print("\nDataFrame carregado!\n")
-    
 
+    #mostra as primeiras 5 linhas
+    print("\nPrimeiras 5 linhas:\n")
+    print(df.head())
+
+    #mostra os nomes das colunas
+    print("\nNomes das colunas:\n")
+    print(df.columns.to_list()) #df.columns sozinho retorna um objeto do pandas. O to_list transforma em uma lista simples de py.
+
+    #mostra tipos e quantidade de valores 
+    print("\nInformações sobre a base:\n")
+    print(df.info())
+
+    #mostra estatísticas das colunas numéricas
+    print("\nEstatísticas descritivas:\n")
+    print(df.describe())
+
+    print("\nDimensão da base:\n")
+    print(df.shape)
+          
 
 if __name__ == "__main__":
     main()
